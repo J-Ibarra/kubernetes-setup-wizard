59e0bedcc8ecca1006c5dbfd667aa4d76945db06
Juan Ibarra <ibarrajj@gmail.com>
2018-06-21 23:00:03 -0400

    setup etcd

---
+++ etcd/etcd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ETCD_VERSION=v3.3.8
+
+mkdir -p /var/lib/k8s/etcd/data
+groupadd etcd
+useradd etcd -d /var/lib/k8s/etcd -s /bin/false -g etcd
+chown etcd:etcd /var/lib/k8s/etcd -R
+wget -P /tmp/ https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
+tar -xzf /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C /tmp/
+chmod +x -R /tmp/etcd-${ETCD_VERSION}-linux-amd64
+mv /tmp/etcd-${ETCD_VERSION}-linux-amd64/etcd /usr/bin/etcd
+mv /tmp/etcd-${ETCD_VERSION}-linux-amd64/etcdctl /usr/bin/etcdctl
+
+cp etcd.conf.yml.example /var/lib/k8s/etcd/etcd.conf.yml.example
+cp etcd.conf.yml.example /var/lib/k8s/etcd/etcd.conf.yml
+
+cp etcd.service /lib/systemd/system/etcd.service
+
+systemctl daemon-reload
+systemctl enable etcd.service
